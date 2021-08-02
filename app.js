421a59a0d6c05fd83f99e985196cc96c2738266d
KOMLATSE KOMLA DODJI <john@togoenvogue.com>
2021-08-02 07:56:02 +0000

    Listen on port 8080

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'views', 'welcome.html');
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "views", "welcome.html");
   res.sendFile(filePath);
 });
 
-app.listen(80);
+app.listen(8080);
